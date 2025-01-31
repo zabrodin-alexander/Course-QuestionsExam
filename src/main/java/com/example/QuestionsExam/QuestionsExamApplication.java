8b5edcb173fb3bb1fc21a737b94f1e88f71d9297
Zabro <zabrodin.alexander51@gmail.com>
2025-01-31 15:24:44 +0300

    begin

---
+++ src/main/java/com/example/QuestionsExam/QuestionsExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuestionsExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsExamApplication.class, args);
+	}
+
+}
